a60eabf2094257701f7c93d20daca11839f83c03
atonyma7 <atonyma7@gmail.com>
2022-11-17 09:37:18 -0700

    Removing unused files

--- Game.h
+++ Game.h
@@ -6,22 +6,6 @@
 
 using namespace sc2;
 
-class Army : Unit {
-private:
-    bool isStructure();
-    enum mobsron {
-        MINERAL_GATHERER,
-        VESPENE_GATHERER,
-        BUILDER,
-        GROUND_MOBRON_A,
-        GROUND_MOBRON_B,
-        GROUND_MOBRON_C,
-        AIR_MOBRON
-    };
-    int value; // the cost to build the unit (minerals + vespene)
-    ActionRaw currentAction;
-};
-
 class GameStatus { // rough pseudocode
 private:
     int value_lost; // sum of the value of all units/structures lost
