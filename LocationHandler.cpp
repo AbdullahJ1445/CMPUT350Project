2868ca0678f4b8407147da88109b073a006ccebb
atonyma7 <atonyma7@gmail.com>
2022-11-17 18:17:01 -0700

    oni# Please enter the commit message for your changes. Lines starting

--- LocationHandler.cpp
+++ LocationHandler.cpp
@@ -31,8 +31,8 @@ int LocationHandler::getIndexOfClosestBase(sc2::Point2D location_) {
 // get the index of the closest base to a location
     float distance = std::numeric_limits<float>::max();
     int lowest_index = 0;
-    for (int i = 0; i < agent->bases.size(); i++) {
-        float b_dist = sc2::DistanceSquared2D(agent->bases[i].get_townhall(), location_);
+    for (int i = 0; i < bases.size(); i++) {
+        float b_dist = sc2::DistanceSquared2D(bases[i].get_townhall(), location_);
         if (b_dist < distance) {
             lowest_index = i;
             distance = b_dist;
@@ -166,43 +166,43 @@ void LocationHandler::initLocations(int map_index, int p_id) {
         {
             Base main_base(observation->GetStartLocation());
             Base exp_1(66.5, 161.5);
-            agent->bases.push_back(exp_1);
+            bases.push_back(exp_1);
 
             Base exp_2(54.5, 132.5);
-            agent->bases.push_back(exp_2);
+            bases.push_back(exp_2);
 
             Base exp_3(45.5, 20.5);
-            agent->bases.push_back(exp_3);
+            bases.push_back(exp_3);
 
             Base exp_4(93.5, 156.5);
-            agent->bases.push_back(exp_4);
+            bases.push_back(exp_4);
 
             Base exp_5(35.5, 93.5);
-            agent->bases.push_back(exp_5);
+            bases.push_back(exp_5);
 
             Base exp_6(30.5, 66.5);
-            agent->bases.push_back(exp_6);
+            bases.push_back(exp_6);
 
             Base exp_7(132.5, 137.5);
-            agent->bases.push_back(exp_7);
+            bases.push_back(exp_7);
 
             Base exp_8(59.5, 54.5);
-            agent->bases.push_back(exp_8);
+            bases.push_back(exp_8);
 
             Base exp_9(161.5, 125.5);
-            agent->bases.push_back(exp_9);
+            bases.push_back(exp_9);
 
             Base exp_10(156.5, 98.5);
-            agent->bases.push_back(exp_10);
+            bases.push_back(exp_10);
 
             Base exp_11(98.5, 35.5);
-            agent->bases.push_back(exp_11);
+            bases.push_back(exp_11);
 
             Base exp_12(125.5, 30.5);
-            agent->bases.push_back(exp_12);
+            bases.push_back(exp_12);
 
             Base exp_13(137.5, 59.5);
-            agent->bases.push_back(exp_13);
+            bases.push_back(exp_13);
             break;
         }
         break;
@@ -213,62 +213,62 @@ void LocationHandler::initLocations(int map_index, int p_id) {
         {
             agent->enemy_location = sc2::Point2D(114.5, 25.5);
             Base main_base(observation->GetStartLocation());
-            agent->bases.push_back(main_base);
+            bases.push_back(main_base);
 
             Base exp_1(82.5, 23.5);
-            agent->bases.push_back(exp_1);
+            bases.push_back(exp_1);
 
             Base exp_2(115.5, 63.5);
-            agent->bases.push_back(exp_2);
+            bases.push_back(exp_2);
 
             Base exp_3(45.5, 20.5);
-            agent->bases.push_back(exp_3);
+            bases.push_back(exp_3);
 
             Base exp_4(120.5, 104.5);
-            agent->bases.push_back(exp_4);
+            bases.push_back(exp_4);
 
             Base exp_5(98.5, 138.5);
-            agent->bases.push_back(exp_5);
+            bases.push_back(exp_5);
 
             Base exp_6(23.5, 55.5);
-            agent->bases.push_back(exp_6);
+            bases.push_back(exp_6);
 
             Base exp_7(28.5, 96.5);
-            agent->bases.push_back(exp_7);
+            bases.push_back(exp_7);
 
             Base exp_8(61.5, 136.5);
-            agent->bases.push_back(exp_8);
+            bases.push_back(exp_8);
             break;
         }
         case 2:
         {
             agent->enemy_location = sc2::Point2D(29.5, 134.5);
             Base main_base(observation->GetStartLocation());
-            agent->bases.push_back(main_base);
+            bases.push_back(main_base);
 
             Base exp_1(61.5, 136.5);
-            agent->bases.push_back(exp_1);
+            bases.push_back(exp_1);
 
             Base exp_2(28.5, 96.5);
-            agent->bases.push_back(exp_2);
+            bases.push_back(exp_2);
 
             Base exp_3(23.5, 55.5);
-            agent->bases.push_back(exp_3);
+            bases.push_back(exp_3);
 
             Base exp_4(98.5, 138.5);
-            agent->bases.push_back(exp_4);
+            bases.push_back(exp_4);
 
             Base exp_5(120.5, 104.5);
-            agent->bases.push_back(exp_5);
+            bases.push_back(exp_5);
 
             Base exp_6(45.5, 20.5);
-            agent->bases.push_back(exp_6);
+            bases.push_back(exp_6);
 
             Base exp_7(115.5, 63.5);
-            agent->bases.push_back(exp_7);
+            bases.push_back(exp_7);
 
             Base exp_8(82.5, 23.5);
-            agent->bases.push_back(exp_8);
+            bases.push_back(exp_8);
             break;
         }
         break;
@@ -285,73 +285,73 @@ void LocationHandler::initLocations(int map_index, int p_id) {
             main_base.add_build_area(132.0, 132.0);
             main_base.add_defend_point(149.0, 120.0);
             main_base.set_active();
-            agent->bases.push_back(main_base);
+            bases.push_back(main_base);
 
             Base exp_1(164.5, 140.5);
             exp_1.add_build_area(165.0, 135.0);
             exp_1.add_defend_point(168.0, 132.0);
-            agent->bases.push_back(exp_1);
+            bases.push_back(exp_1);
 
             Base exp_2(149.5, 102.5);
             exp_2.add_build_area(144.0, 106.0);
             exp_2.add_defend_point(141.0, 95.0);
-            agent->bases.push_back(exp_2);
+            bases.push_back(exp_2);
 
             Base exp_3(166.5, 69.5);
             exp_3.add_build_area(161.0, 79.0);
             exp_3.add_defend_point(155.0, 69.0);
-            agent->bases.push_back(exp_3);
+            bases.push_back(exp_3);
 
             Base exp_4(119.5, 111.5);
             exp_4.add_defend_point(115.0, 105.0);
             exp_4.add_defend_point(110.0, 115.0);
-            agent->bases.push_back(exp_4);
+            bases.push_back(exp_4);
 
             Base exp_5(127.5, 57.5);
             exp_5.add_defend_point(122.0, 69.0);
             exp_5.add_build_area(137.0, 54.0);
-            agent->bases.push_back(exp_5);
+            bases.push_back(exp_5);
 
             Base exp_6(165.5, 23.5);
             exp_6.add_defend_point(157.0, 23.0);
-            agent->bases.push_back(exp_6);
+            bases.push_back(exp_6);
 
             Base exp_7(93.5, 147.5);
             exp_7.add_defend_point(94.0, 141.0);
-            agent->bases.push_back(exp_7);
+            bases.push_back(exp_7);
 
             Base exp_8(106.5, 20.5);
             exp_8.add_defend_point(106.0, 27.0);
-            agent->bases.push_back(exp_8);
+            bases.push_back(exp_8);
 
             Base exp_9(34.5, 144.5);
             exp_9.add_defend_point(43.0, 145.0);
-            agent->bases.push_back(exp_9);
+            bases.push_back(exp_9);
 
             Base exp_10(72.5, 110.5);
             exp_10.add_defend_point(78.0, 99.0);
             exp_10.add_defend_point(63.0, 114.0);
-            agent->bases.push_back(exp_10);
+            bases.push_back(exp_10);
 
             Base exp_11(80.5, 56.5);
             exp_11.add_defend_point(85.0, 63.0);
             exp_11.add_defend_point(90.0, 53.0);
-            agent->bases.push_back(exp_11);
+            bases.push_back(exp_11);
 
             Base exp_12(33.5, 98.5);
             exp_12.add_build_area(39.0, 89.0);
             exp_12.add_defend_point(45.0, 99.0);
-            agent->bases.push_back(exp_12);
+            bases.push_back(exp_12);
 
             Base exp_13(50.5, 65.5);
             exp_13.add_build_area(56.0, 62.0);
             exp_13.add_defend_point(59.0, 73.0);
-            agent->bases.push_back(exp_13);
+            bases.push_back(exp_13);
 
             Base exp_14(35.5, 27.5);
             exp_14.add_build_area(35.0, 33.0);
             exp_14.add_defend_point(32.0, 36.0);
-            agent->bases.push_back(exp_14);
+            bases.push_back(exp_14);
 
             break;
         }
@@ -364,73 +364,73 @@ void LocationHandler::initLocations(int map_index, int p_id) {
             main_base.add_build_area(68.0, 36.0);
             main_base.add_defend_point(51.0, 48.0);
             main_base.set_active();
-            agent->bases.push_back(main_base);
+            bases.push_back(main_base);
 
             Base exp_1(35.5, 27.5);
             exp_1.add_build_area(35.0, 33.0);
             exp_1.add_defend_point(32.0, 36.0);
-            agent->bases.push_back(exp_1);
+            bases.push_back(exp_1);
 
             Base exp_2(50.5, 65.5);
             exp_2.add_build_area(56.0, 62.0);
             exp_2.add_defend_point(59.0, 73.0);
-            agent->bases.push_back(exp_2);
+            bases.push_back(exp_2);
 
             Base exp_3(33.5, 98.5);
             exp_3.add_build_area(39.0, 89.0);
             exp_3.add_defend_point(45.0, 99.0);
-            agent->bases.push_back(exp_3);
+            bases.push_back(exp_3);
 
             Base exp_4(80.5, 56.5);
             exp_4.add_defend_point(85.0, 63.0);
             exp_4.add_defend_point(90.0, 53.0);
-            agent->bases.push_back(exp_4);
+            bases.push_back(exp_4);
 
             Base exp_5(72.5, 110.5);
             exp_5.add_defend_point(78.0, 99.0);
             exp_5.add_defend_point(63.0, 114.0);
-            agent->bases.push_back(exp_5);
+            bases.push_back(exp_5);
 
             Base exp_6(34.5, 144.5);
             exp_6.add_defend_point(43.0, 145.0);
-            agent->bases.push_back(exp_6);
+            bases.push_back(exp_6);
 
             Base exp_7(106.5, 20.5);
             exp_7.add_defend_point(106.0, 27.0);
-            agent->bases.push_back(exp_7);
+            bases.push_back(exp_7);
 
             Base exp_8(93.5, 147.5);
             exp_8.add_defend_point(94.0, 141.0);
-            agent->bases.push_back(exp_8);
+            bases.push_back(exp_8);
 
             Base exp_9(165.5, 23.5);
             exp_9.add_defend_point(157.0, 23.0);
-            agent->bases.push_back(exp_9);
+            bases.push_back(exp_9);
 
             Base exp_10(127.5, 57.5);
             exp_10.add_defend_point(122.0, 69.0);
             exp_10.add_build_area(137.0, 54.0);
-            agent->bases.push_back(exp_10);
+            bases.push_back(exp_10);
 
             Base exp_11(119.5, 111.5);
             exp_11.add_defend_point(115.0, 105.0);
             exp_11.add_defend_point(110.0, 115.0);
-            agent->bases.push_back(exp_11);
+            bases.push_back(exp_11);
 
             Base exp_12(166.5, 69.5);
             exp_12.add_build_area(161.0, 79.0);
             exp_12.add_defend_point(155.0, 69.0);
-            agent->bases.push_back(exp_12);
+            bases.push_back(exp_12);
 
             Base exp_13(149.5, 102.5);
             exp_13.add_build_area(144.0, 106.0);
             exp_13.add_defend_point(141.0, 95.0);
-            agent->bases.push_back(exp_13);
+            bases.push_back(exp_13);
 
             Base exp_14(164.5, 140.5);
             exp_14.add_build_area(165.0, 135.0);
             exp_14.add_defend_point(168.0, 132.0);
-            agent->bases.push_back(exp_14);
+            bases.push_back(exp_14);
 
             break;
         }
