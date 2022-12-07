6b44c501f9babdccab08040f1b3645434f3d0540
Abdullah Mohammed <37706742+AbdullahJ1445@users.noreply.github.com>
2022-12-07 13:06:53 -0700

    CC, AO, Deconstructor

--- Strategy.h
+++ Strategy.h
@@ -3,7 +3,10 @@
 #include "sc2api/sc2_api.h"
 
 struct Strategy {
-	Strategy(BasicSc2Bot* bot_);
+	Strategy(BasicSc2Bot* bot_); // constructor
+	Strategy(const Strategy& rhs);	// copy constructor
+	Strategy& operator=(const Strategy& rhs);	// copy assignment operator
+	~Strategy();	// destructor
 	void loadGameSettings(int *map_index, sc2::Race *bot_race, sc2::Race *opp_race, sc2::Difficulty *difficulty, bool *human_player=false, bool *fullscreen=false, bool *realtime=false);
 	void loadStrategies();
 
